8793836b223ab6d6a92bbb3e491f935e1a570a6f
Joel Gustafson <joelg@mit.edu>
2017-01-22 15:31:35 -0500

    cd to /files/ on init

--- scripts/start-scheme.sh
+++ scripts/start-scheme.sh
@@ -3,4 +3,4 @@
 user=$1
 uuid=$2
 
-exec schroot -c scheme -d / -o user.path=${user} -- /bin/bash /bin/start /pipes/${uuid}
+exec schroot -c scheme -d /files -o user.path=${user} -- /bin/bash /bin/start /pipes/${uuid}
--- utils/load.scm
+++ utils/load.scm
@@ -1,9 +1,9 @@
-(load "utils/json")
-(load "utils/output")
-(load "utils/print")
-(load "utils/error")
-(load "utils/stack")
+(load "/utils/json")
+(load "/utils/output")
+(load "/utils/print")
+(load "/utils/error")
+(load "/utils/stack")
 
-(load "utils/canvas/canvas")
-(load "utils/canvas/install")
+(load "/utils/canvas/canvas")
+(load "/utils/canvas/install")
 
